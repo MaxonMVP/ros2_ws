d9dfa3ad67472c573cb8c31f3d5c16ee5ea55580
MaxonMVP <peitch48@gmail.com>
2026-03-26 21:04:24 +0200

    update

---
+++ src/my_robot_controller/my_robot_controller/mapping.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+class TurtlebotMappingNode(Node):
+    def __init__(self):
+        super().__init__("mapping_node")
+
+        self.set_parameters([rclpy.parameter.Parameter('use_sim_time', rclpy.Parameter.Type.BOOL, True)])
+        
+        self.get_logger().info("Mapping Node has started.")
+        # Publisher to send movement commands
+        self._pose_publisher = self.create_publisher(
+            Twist, "/cmd_vel", 10
+        )
+
+        # Subscriber to receive LiDAR data
+        self._scan_listener = self.create_subscription(
+            LaserScan, "/scan", self.robot_controller, 12
+        )
+
+    def robot_controller(self, scan: LaserScan):
+        cmd = Twist()
+        # Define the width of the range for obstacle detection
+        ranges = [r if (r > scan.range_min and r < scan.range_max) else scan.range_max for r in scan.ranges]
+        a = 7  
+
+        # Extract directional distances
+        self._front = min((scan.ranges[:a+1] + scan.ranges[-a:]))
+        self._left = min(scan.ranges[90-a:90+a+1])
+        self._right = min(scan.ranges[270-a:270+a+1])
+
+        # Navigation logic based on obstacle detection
+        if self._front < 1.0:  # Obstacle ahead
+            if self._right < self._left:
+                cmd.linear.x = 0.005
+                cmd.angular.z = 0.15  # Turn left
+            else:
+                cmd.linear.x = 0.005
+                cmd.angular.z = -0.15  # Turn right
+        else:
+            cmd.linear.x = 0.5
+            cmd.angular.z = 0.0  # Move forward
+
+        # Publish the command
+        self._pose_publisher.publish(cmd)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TurtlebotMappingNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
--- src/my_robot_controller/setup.py
+++ src/my_robot_controller/setup.py
@@ -1,7 +1,6 @@
 from setuptools import find_packages, setup
 from glob import glob
 
-
 package_name = 'my_robot_controller'
 
 setup(
@@ -13,6 +12,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/maps', glob('maps/*')),
+        ('share/' + package_name + '/worlds', glob('worlds/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,16 +21,16 @@ setup(
     maintainer_email='student@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'test_node = my_robot_controller.my_first_node:main',
-        'draw_circle = my_robot_controller.draw_circle:main',
-        'turtle_controller = my_robot_controller.turtle_controller:main',
+            'test_node = my_robot_controller.my_first_node:main',
+            'draw_circle = my_robot_controller.draw_circle:main',
+            'pose_sub = my_robot_controller.pose_sub:main',
+            'turtle_control = my_robot_controller.turtle_controller:main',
+            'mapping = my_robot_controller.mapping:main',
+            'navigation = my_robot_controller.navigation:main',
+            'av_nav = my_robot_controller.av_navigation:main', 
         ],
     },
 )
